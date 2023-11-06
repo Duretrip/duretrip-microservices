50e3148b5a08b982531f36d7c4c96f3bcb6145e9
Damilare Ademeso <ademesodamilare@gmail.com>
2023-11-06 11:18:38 +0100

    working auth and API gateway

--- services/api-gateway/src/app.controller.ts
+++ services/api-gateway/src/app.controller.ts
@@ -1,6 +1,7 @@
 import { All, Controller, Req, Get } from '@nestjs/common';
 import axios from 'axios'; // Import Axios
 import { Request } from 'express';
+import * as url from 'url';
 
 @Controller('api/v1')
 export class AppController {
@@ -10,14 +11,25 @@ export class AppController {
   async proxyToAuthService(@Req() req: Request) {
     const AUTH_SERVICE_HOSTNAME = process.env.AUTH_SERVICE_HOSTNAME || 'localhost';
     const AUTH_SERVICE_PORT = process.env.AUTH_SERVICE_PORT || 8000;
-    const authServiceURL = `http://${AUTH_SERVICE_HOSTNAME}:${AUTH_SERVICE_PORT}`;
-    // const requestURL = `${authServiceURL}/${req.params[0]}`;
-    const requestURL = `${authServiceURL}/${req.params[0]}?_=${new Date().getTime()}`;
+    // Parse the request URL
+    // Parse the URL
+    const originalUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+    const newUrl = originalUrl.replace('/api/v1/auth-service', '');
+    
+    const url = new URL(newUrl);
+
+    // Get the parts you need
+    const path = url.pathname;  // Get the path, e.g., "/path/to/resource"
+    const query = url.search;   // Get the query string, e.g., "?query=value"
+    const fragment = url.hash; // Get the fragment, e.g., "#fragment"
+
+    // Build the modified URL
+    const newUrlString = `http://${AUTH_SERVICE_HOSTNAME}${AUTH_SERVICE_PORT ? `:${AUTH_SERVICE_PORT}` : ""}${path}${query}${fragment}`;
 
     try {
       const response = await axios({
         method: req.method,
-        url: requestURL,
+        url: newUrlString,
         headers: req.headers,
         data: req.body,
       });
