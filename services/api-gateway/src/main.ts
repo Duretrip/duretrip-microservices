f7a1cac02fb51a3d86f3e003c43b3694b30c17d4
Damilare Ademeso <ademesodamilare@gmail.com>
2023-11-07 18:46:11 +0100

    added a globsl exception filter to the app

--- services/api-gateway/src/main.ts
+++ services/api-gateway/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './allExceptionsFilter';
 require('dotenv').config();
 
 const port = process.env.APP_PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new AllExceptionsFilter());
+
   await app.listen(port);
   console.log(`app listening on ${port}`)
 }
