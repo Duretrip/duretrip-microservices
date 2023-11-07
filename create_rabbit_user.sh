3a0881e76ad153939e5e9194d1deee4067c9a7cd
Damilare Ademeso <ademesodamilare@gmail.com>
2023-11-07 11:29:52 +0100

    cleaned up rabbitmq

--- create_rabbit_user.sh
+++
@@ -1,6 +0,0 @@
-# Generate the password hash
-hashed_password=$(rabbitmqctl eval 'io:format("~s~n", [rabbit_auth_backend_internal:hash_password(<<"Zijela2023">>)]).')
-
-sudo rabbitmqctl add_user Zijela $hashed_password
-sudo rabbitmqctl set_user_tags Zijela administrator
-sudo rabbitmqctl set_permissions -p / Zijela ".*" ".*" ".*"
