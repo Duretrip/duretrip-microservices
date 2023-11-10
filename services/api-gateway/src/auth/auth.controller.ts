1ffc7b978d634b8297a686a7d59070988eb1fd89
Ziggybaba1 <seyiadejugbagbe@gmail.com>
2023-11-10 11:56:41 +0100

    fix server

--- services/api-gateway/src/auth/auth.controller.ts
+++ services/api-gateway/src/auth/auth.controller.ts
@@ -4,129 +4,246 @@ import {
   Get,
   HttpCode,
   HttpStatus,
-  Request,
+  Param,
   Post,
+  Put,
+  Query,
+  Req,
+  Res,
+  UploadedFile,
   UseGuards,
-  Patch,
-  Delete,
-  SerializeOptions,
+  UseInterceptors,
+  ValidationPipe
 } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { AuthEmailLoginDto } from './dto/auth-email-login.dto';
-import { AuthForgotPasswordDto } from './dto/auth-forgot-password.dto';
-import { AuthConfirmEmailDto } from './dto/auth-confirm-email.dto';
-import { AuthResetPasswordDto } from './dto/auth-reset-password.dto';
-import { AuthUpdateDto } from './dto/auth-update.dto';
-import { AuthGuard } from '@nestjs/passport';
-import { AuthRegisterLoginDto } from './dto/auth-register-login.dto';
-import { LoginResponseType } from './types/login-response.type';
-import { User } from '../users/entities/user.entity';
-import { NullableType } from '../utils/types/nullable.type';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiTags } from '@nestjs/swagger';
+import { Request, Response } from 'express';
+import { UAParser } from 'ua-parser-js';
 
-@ApiTags('Auth')
-@Controller({
-  path: 'auth',
-  version: '1',
-})
+import { GetUser } from 'src/common/decorators/get-user.decorator';
+import JwtTwoFactorGuard from 'src/common/guard/jwt-two-factor.guard';
+import { PermissionGuard } from 'src/common/guard/permission.guard';
+import { multerOptionsHelper } from 'src/common/helper/multer-options.helper';
+import { Pagination } from 'src/paginate';
+import { RefreshToken } from 'src/refresh-token/entities/refresh-token.entity';
+import { AuthService } from 'src/auth/auth.service';
+import { ChangePasswordDto } from 'src/auth/dto/change-password.dto';
+import { CreateUserDto } from 'src/auth/dto/create-user.dto';
+import { ForgetPasswordDto } from 'src/auth/dto/forget-password.dto';
+import { RegisterUserDto } from 'src/auth/dto/register-user.dto';
+import { ResetPasswordDto } from 'src/auth/dto/reset-password.dto';
+import { UpdateUserProfileDto } from 'src/auth/dto/update-user-profile.dto';
+import { UpdateUserDto } from 'src/auth/dto/update-user.dto';
+import { UserLoginDto } from 'src/auth/dto/user-login.dto';
+import { UserSearchFilterDto } from 'src/auth/dto/user-search-filter.dto';
+import { UserEntity } from 'src/auth/entity/user.entity';
+import { UserSerializer } from 'src/auth/serializer/user.serializer';
+import { RefreshPaginateFilterDto } from 'src/refresh-token/dto/refresh-paginate-filter.dto';
+import { RefreshTokenSerializer } from 'src/refresh-token/serializer/refresh-token.serializer';
+
+@ApiTags('user')
+@Controller()
 export class AuthController {
-  constructor(private readonly service: AuthService) {}
+  constructor(private authService: AuthService) {}
 
-  @SerializeOptions({
-    groups: ['me'],
-  })
-  @Post('email/login')
-  @HttpCode(HttpStatus.OK)
-  public login(
-    @Body() loginDto: AuthEmailLoginDto,
-  ): Promise<LoginResponseType> {
-    return this.service.validateLogin(loginDto);
+  @Post('/auth/register')
+  register(
+    @Body(ValidationPipe)
+    registerUserDto: RegisterUserDto
+  ): Promise<UserSerializer> {
+    return this.authService.create(registerUserDto);
   }
 
-  @Post('email/register')
-  @HttpCode(HttpStatus.NO_CONTENT)
-  async register(@Body() createUserDto: AuthRegisterLoginDto): Promise<void> {
-    return await this.service.register(createUserDto);
-  }
-
-  @Post('email/confirm')
-  @HttpCode(HttpStatus.NO_CONTENT)
-  async confirmEmail(
-    @Body() confirmEmailDto: AuthConfirmEmailDto,
-  ): Promise<void> {
-    return await this.service.confirmEmail(confirmEmailDto.hash);
-  }
-
-  @Post('forgot/password')
-  @HttpCode(HttpStatus.NO_CONTENT)
-  async forgotPassword(
-    @Body() forgotPasswordDto: AuthForgotPasswordDto,
-  ): Promise<void> {
-    return this.service.forgotPassword(forgotPasswordDto.email);
-  }
-
-  @Post('reset/password')
-  @HttpCode(HttpStatus.NO_CONTENT)
-  resetPassword(@Body() resetPasswordDto: AuthResetPasswordDto): Promise<void> {
-    return this.service.resetPassword(
-      resetPasswordDto.hash,
-      resetPasswordDto.password,
+  @Post('/auth/login')
+  async login(
+    @Req()
+    req: Request,
+    @Res()
+    response: Response,
+    @Body()
+    userLoginDto: UserLoginDto
+  ) {
+    const ua = UAParser(req.headers['user-agent']);
+    const refreshTokenPayload: Partial<RefreshToken> = {
+      ip: req.ip,
+      userAgent: JSON.stringify(ua),
+      browser: ua.browser.name,
+      os: ua.os.name
+    };
+    const cookiePayload = await this.authService.login(
+      userLoginDto,
+      refreshTokenPayload
     );
+    response.setHeader('Set-Cookie', cookiePayload);
+    return response.status(HttpStatus.NO_CONTENT).json({});
   }
 
-  @ApiBearerAuth()
-  @SerializeOptions({
-    groups: ['me'],
-  })
-  @Get('me')
-  @UseGuards(AuthGuard('jwt'))
-  @HttpCode(HttpStatus.OK)
-  public me(@Request() request): Promise<NullableType<User>> {
-    return this.service.me(request.user);
+  @Post('/refresh')
+  async refresh(
+    @Req()
+    req: Request,
+    @Res()
+    response: Response
+  ) {
+    try {
+      const cookiePayload =
+        await this.authService.createAccessTokenFromRefreshToken(
+          req.cookies['Refresh']
+        );
+      response.setHeader('Set-Cookie', cookiePayload);
+      return response.status(HttpStatus.NO_CONTENT).json({});
+    } catch (e) {
+      response.setHeader('Set-Cookie', this.authService.getCookieForLogOut());
+      return response.sendStatus(HttpStatus.BAD_REQUEST);
+    }
   }
 
-  @ApiBearerAuth()
-  @SerializeOptions({
-    groups: ['me'],
-  })
-  @Post('refresh')
-  @UseGuards(AuthGuard('jwt-refresh'))
-  @HttpCode(HttpStatus.OK)
-  public refresh(@Request() request): Promise<Omit<LoginResponseType, 'user'>> {
-    return this.service.refreshToken({
-      sessionId: request.user.sessionId,
-    });
-  }
-
-  @ApiBearerAuth()
-  @Post('logout')
-  @UseGuards(AuthGuard('jwt'))
+  @Get('/auth/activate-account')
   @HttpCode(HttpStatus.NO_CONTENT)
-  public async logout(@Request() request): Promise<void> {
-    await this.service.logout({
-      sessionId: request.user.sessionId,
-    });
+  activateAccount(
+    @Query('token')
+    token: string
+  ): Promise<void> {
+    return this.authService.activateAccount(token);
   }
 
-  @ApiBearerAuth()
-  @SerializeOptions({
-    groups: ['me'],
-  })
-  @Patch('me')
-  @UseGuards(AuthGuard('jwt'))
-  @HttpCode(HttpStatus.OK)
-  public update(
-    @Request() request,
-    @Body() userDto: AuthUpdateDto,
-  ): Promise<NullableType<User>> {
-    return this.service.update(request.user, userDto);
-  }
-
-  @ApiBearerAuth()
-  @Delete('me')
-  @UseGuards(AuthGuard('jwt'))
+  @Put('/auth/forgot-password')
   @HttpCode(HttpStatus.NO_CONTENT)
-  public async delete(@Request() request): Promise<void> {
-    return this.service.softDelete(request.user);
+  forgotPassword(
+    @Body()
+    forgetPasswordDto: ForgetPasswordDto
+  ): Promise<void> {
+    return this.authService.forgotPassword(forgetPasswordDto);
+  }
+
+  @Put('/auth/reset-password')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  resetPassword(
+    @Body()
+    resetPasswordDto: ResetPasswordDto
+  ): Promise<void> {
+    return this.authService.resetPassword(resetPasswordDto);
+  }
+
+  @UseGuards(JwtTwoFactorGuard)
+  @Get('/auth/profile')
+  profile(
+    @GetUser()
+    user: UserEntity
+  ): Promise<UserSerializer> {
+    return this.authService.get(user);
+  }
+
+  @UseGuards(JwtTwoFactorGuard)
+  @Put('/auth/profile')
+  @UseInterceptors(
+    FileInterceptor(
+      'avatar',
+      multerOptionsHelper('public/images/profile', 1000000)
+    )
+  )
+  updateProfile(
+    @GetUser()
+    user: UserEntity,
+    @UploadedFile()
+    file: Express.Multer.File,
+    @Body()
+    updateUserDto: UpdateUserProfileDto
+  ): Promise<UserSerializer> {
+    if (file) {
+      updateUserDto.avatar = file.filename;
+    }
+    return this.authService.update(user.id, updateUserDto);
+  }
+
+  @UseGuards(JwtTwoFactorGuard)
+  @Put('/auth/change-password')
+  changePassword(
+    @GetUser()
+    user: UserEntity,
+    @Body()
+    changePasswordDto: ChangePasswordDto
+  ): Promise<void> {
+    return this.authService.changePassword(user, changePasswordDto);
+  }
+
+  @UseGuards(JwtTwoFactorGuard, PermissionGuard)
+  @Get('/users')
+  findAll(
+    @Query()
+    userSearchFilterDto: UserSearchFilterDto
+  ): Promise<Pagination<UserSerializer>> {
+    return this.authService.findAll(userSearchFilterDto);
+  }
+
+  @UseGuards(JwtTwoFactorGuard, PermissionGuard)
+  @Post('/users')
+  create(
+    @Body(ValidationPipe)
+    createUserDto: CreateUserDto
+  ): Promise<UserSerializer> {
+    return this.authService.create(createUserDto);
+  }
+
+  @UseGuards(JwtTwoFactorGuard, PermissionGuard)
+  @Put('/users/:id')
+  update(
+    @Param('id')
+    id: string,
+    @Body()
+    updateUserDto: UpdateUserDto
+  ): Promise<UserSerializer> {
+    return this.authService.update(+id, updateUserDto);
+  }
+
+  @UseGuards(JwtTwoFactorGuard, PermissionGuard)
+  @Get('/users/:id')
+  findOne(
+    @Param('id')
+    id: string
+  ): Promise<UserSerializer> {
+    return this.authService.findById(+id);
+  }
+
+  @Post('/logout')
+  async logOut(
+    @Req()
+    req: Request,
+    @Res()
+    response: Response
+  ) {
+    try {
+      const cookie = req.cookies['Refresh'];
+      response.setHeader('Set-Cookie', this.authService.getCookieForLogOut());
+      const refreshCookie = req.cookies['Refresh'];
+      if (refreshCookie) {
+        await this.authService.revokeRefreshToken(cookie);
+      }
+      return response.sendStatus(HttpStatus.NO_CONTENT);
+    } catch (e) {
+      return response.sendStatus(HttpStatus.NO_CONTENT);
+    }
+  }
+
+  @UseGuards(JwtTwoFactorGuard)
+  @Get('/auth/token-info')
+  getRefreshToken(
+    @Query()
+    filter: RefreshPaginateFilterDto,
+    @GetUser()
+    user: UserEntity
+  ): Promise<Pagination<RefreshTokenSerializer>> {
+    return this.authService.activeRefreshTokenList(+user.id, filter);
+  }
+
+  @UseGuards(JwtTwoFactorGuard)
+  @Put('/revoke/:id')
+  revokeToken(
+    @Param('id')
+    id: string,
+    @GetUser()
+    user: UserEntity
+  ) {
+    return this.authService.revokeTokenById(+id, user.id);
   }
 }
