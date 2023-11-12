c2611caaefd6a69044f0a87fb3ea1d82729d6196
Damilare Ademeso <ademesodamilare@gmail.com>
2023-11-12 13:12:36 +0100

    troubleshooting with seyi again

--- init.sql
+++ init.sql
@@ -4,15 +4,15 @@ CREATE DATABASE jet_db;
 CREATE DATABASE hotel_db;
 
 -- Create users and grant privileges
-CREATE USER "dureauth" WITH ENCRYPTED PASSWORD 'dureAuth2023';
+CREATE USER "dureauth" WITH ENCRYPTED PASSWORD '${PASSWORDE}';
 GRANT ALL PRIVILEGES ON DATABASE auth_dbb TO "dureauth";
 
 -- Create users and grant privileges
-CREATE USER "durejet" WITH ENCRYPTED PASSWORD 'dureJet2023';
+CREATE USER "durejet" WITH ENCRYPTED PASSWORD '${PASSWORDE}';
 GRANT ALL PRIVILEGES ON DATABASE jet_db TO "durejet";
 
 -- Create users and grant privileges
-CREATE USER "durehotel" WITH ENCRYPTED PASSWORD 'dureHotel2023';
+CREATE USER "durehotel" WITH ENCRYPTED PASSWORD '${PASSWORDE}';
 GRANT ALL PRIVILEGES ON DATABASE hotel_db TO "durehotel";
 
 -- -- init.sql
