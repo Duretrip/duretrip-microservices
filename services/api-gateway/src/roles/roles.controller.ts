eae60d427aef2b471672ffd3719b07f86f91c2ef
Damilare Ademeso <ademesodamilare@gmail.com>
2023-11-12 12:32:51 +0100

    removed env logging

--- services/api-gateway/src/roles/roles.controller.ts
+++ services/api-gateway/src/roles/roles.controller.ts
@@ -2,7 +2,9 @@
 
 import { Controller, Param, ParseIntPipe, Post, Body } from '@nestjs/common';
 import { RolesService } from './roles.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Roles')
 @Controller('roles')
 export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
