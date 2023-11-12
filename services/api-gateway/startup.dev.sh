b4701a244cec4c7b2914982bcb5d02ade2174911
Damilare Ademeso <ademesodamilare@gmail.com>
2023-11-12 13:37:25 +0100

    solved

--- services/api-gateway/startup.dev.sh
+++ services/api-gateway/startup.dev.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # set -e
-RUN chmod +x /opt/startup.docker.sh
+RUN chmod +x /opt/wait-for-it.sh
 
 # /opt/wait-for-it.sh postgres:5432
-sleep 30
+# sleep 30
 
 npm run migration:run
 npm run seed:run
