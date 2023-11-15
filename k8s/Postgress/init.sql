b5f752424c8b197834e81bca5d8ccc4ba091d14c
Damilare Ademeso <ademesodamilare@gmail.com>
2023-11-15 17:40:53 +0100

    devops setup

--- k8s/Postgress/init.sql
+++ k8s/Postgress/init.sql
@@ -1,42 +1,24 @@
 -- Create databases
-CREATE DATABASE auth_dbb;
-CREATE DATABASE jet_db;
-CREATE DATABASE hotel_db;
+CREATE DATABASE {{AUTH_DB_NAME}};
+CREATE DATABASE {{JET_DB_NAME}};
+CREATE DATABASE {{HOTEL_DB_NAME}};
 
 -- Create users and grant privileges
-CREATE USER "dureauth" WITH ENCRYPTED PASSWORD '${PASSWORDE}';
-GRANT ALL PRIVILEGES ON DATABASE auth_dbb TO "dureauth";
+CREATE USER "{{AUTH_DB_USER}}" WITH ENCRYPTED PASSWORD '${PASSWORDE}';
+GRANT ALL PRIVILEGES ON DATABASE {{AUTH_DB_NAME}} TO "{{AUTH_DB_USER}}";
+ALTER USER "{{AUTH_DB_USER}}" WITH CREATEDB;
+GRANT ALL PRIVILEGES ON SCHEMA public TO {{AUTH_DB_USER}};
 
 -- Create users and grant privileges
-CREATE USER "durejet" WITH ENCRYPTED PASSWORD '${PASSWORDE}';
-GRANT ALL PRIVILEGES ON DATABASE jet_db TO "durejet";
+CREATE USER "{{JET_DB_USER}}" WITH ENCRYPTED PASSWORD '${PASSWORDE}';
+GRANT ALL PRIVILEGES ON DATABASE {{JET_DB_NAME}} TO "{{JET_DB_USER}}";
+ALTER USER "{{JET_DB_USER}}" WITH CREATEDB;
+GRANT ALL PRIVILEGES ON SCHEMA public TO {{JET_DB_USER}};
 
 -- Create users and grant privileges
-CREATE USER "durehotel" WITH ENCRYPTED PASSWORD '${PASSWORDE}';
-GRANT ALL PRIVILEGES ON DATABASE hotel_db TO "durehotel";
+CREATE USER "{{HOTEL_DB_USER}}" WITH ENCRYPTED PASSWORD '${PASSWORDE}';
+GRANT ALL PRIVILEGES ON DATABASE {{HOTEL_DB_NAME}} TO "{{HOTEL_DB_USER}}";
+ALTER USER "{{HOTEL_DB_USER}}" WITH CREATEDB;
+GRANT ALL PRIVILEGES ON SCHEMA public TO {{HOTEL_DB_USER}};
 
 -- -- init.sql
-
--- -- Create Database and User for db1
--- CREATE DATABASE db1;
--- CREATE USER db1_user WITH PASSWORD 'db1_password';
--- ALTER ROLE db1_user SET client_encoding TO 'utf8';
--- ALTER ROLE db1_user SET default_transaction_isolation TO 'read committed';
--- ALTER ROLE db1_user SET timezone TO 'UTC';
--- GRANT ALL PRIVILEGES ON DATABASE db1 TO db1_user;
-
--- -- Create Database and User for db2
--- CREATE DATABASE db2;
--- CREATE USER db2_user WITH PASSWORD 'db2_password';
--- ALTER ROLE db2_user SET client_encoding TO 'utf8';
--- ALTER ROLE db2_user SET default_transaction_isolation TO 'read committed';
--- ALTER ROLE db2_user SET timezone TO 'UTC';
--- GRANT ALL PRIVILEGES ON DATABASE db2 TO db2_user;
-
--- -- Create Database and User for db3
--- CREATE DATABASE db3;
--- CREATE USER db3_user WITH PASSWORD 'db3_password';
--- ALTER ROLE db3_user SET client_encoding TO 'utf8';
--- ALTER ROLE db3_user SET default_transaction_isolation TO 'read committed';
--- ALTER ROLE db3_user SET timezone TO 'UTC';
--- GRANT ALL PRIVILEGES ON DATABASE db3 TO db3_user;
--- postgres/init.sql
+++ postgres/init.sql
@@ -15,33 +15,10 @@ ALTER USER "durejet" WITH CREATEDB;
 GRANT ALL PRIVILEGES ON SCHEMA public TO durejet;
 -- GRANT USAGE ON SCHEMA public TO durejet;
 
--- -- Create users and grant privileges
--- CREATE DATABASE hotel_db;
--- CREATE USER "durehotel" WITH ENCRYPTED PASSWORD 'dureHotel2023';
--- GRANT ALL PRIVILEGES ON DATABASE hotel_db TO "durehotel";
-
--- -- init.sql
-
--- -- Create Database and User for db1
--- CREATE DATABASE db1;
--- CREATE USER db1_user WITH PASSWORD 'db1_password';
--- ALTER ROLE db1_user SET client_encoding TO 'utf8';
--- ALTER ROLE db1_user SET default_transaction_isolation TO 'read committed';
--- ALTER ROLE db1_user SET timezone TO 'UTC';
--- GRANT ALL PRIVILEGES ON DATABASE db1 TO db1_user;
-
--- -- Create Database and User for db2
--- CREATE DATABASE db2;
--- CREATE USER db2_user WITH PASSWORD 'db2_password';
--- ALTER ROLE db2_user SET client_encoding TO 'utf8';
--- ALTER ROLE db2_user SET default_transaction_isolation TO 'read committed';
--- ALTER ROLE db2_user SET timezone TO 'UTC';
--- GRANT ALL PRIVILEGES ON DATABASE db2 TO db2_user;
-
--- -- Create Database and User for db3
--- CREATE DATABASE db3;
--- CREATE USER db3_user WITH PASSWORD 'db3_password';
--- ALTER ROLE db3_user SET client_encoding TO 'utf8';
--- ALTER ROLE db3_user SET default_transaction_isolation TO 'read committed';
--- ALTER ROLE db3_user SET timezone TO 'UTC';
--- GRANT ALL PRIVILEGES ON DATABASE db3 TO db3_user;
+-- Create users and grant privileges
+CREATE DATABASE hotel_db;
+CREATE USER "durehotel" WITH ENCRYPTED PASSWORD 'dureHotel2023';
+GRANT ALL PRIVILEGES ON DATABASE hotel_db TO "durehotel";
+ALTER USER "durehotel" WITH CREATEDB;
+GRANT ALL PRIVILEGES ON SCHEMA public TO durehotel;
+-- GRANT USAGE ON SCHEMA public TO durejet;
