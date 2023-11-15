8d2622efc9ceb70416e6bdb91542008ee700c7ac
Damilare Ademeso <ademesodamilare@gmail.com>
2023-11-15 09:36:09 +0100

    asking dare to fix line 28 in jet.service.ts

--- services/jet/prisma/seed.ts
+++ services/jet/prisma/seed.ts
@@ -4,17 +4,17 @@ async function main() {
   try {
     // Range seed Data
     const range1 = await prisma.range.upsert({
-      where: { name: 'Short Range (Up to 2000 Nautical Miles)' },
+      where: { id:1, name: 'Short Range (Up to 2000 Nautical Miles)' },
       update: { name: 'Short Range (Up to 2000 Nautical Miles)' },
       create: { name: 'Short Range (Up to 2000 Nautical Miles)' },
     });
     const range2 = await prisma.range.upsert({
-      where: { name: 'Medium Range (2000-5000 Nautical Miles)' },
+      where: { id:2, name: 'Medium Range (2000-5000 Nautical Miles)' },
       update: { name: 'Medium Range (2000-5000 Nautical Miles)' },
       create: { name: 'Medium Range (2000-5000 Nautical Miles)' },
     });
     const range3 = await prisma.range.upsert({
-      where: { name: 'Long Range (5000+ Nautical Miles)' },
+      where: { id:3, name: 'Long Range (5000+ Nautical Miles)' },
       update: { name: 'Long Range (5000+ Nautical Miles)' },
       create: { name: 'Long Range (5000+ Nautical Miles)' },
     });
@@ -23,27 +23,27 @@ async function main() {
 
     // Facility seed Data
     const facility1 = await prisma.facility.upsert({
-      where: { name: 'Wi-Fi' },
+      where: { id: 4, name: 'Wi-Fi' },
       update: { name: 'Wi-Fi' },
       create: { name: 'Wi-Fi' },
     });
     const facility2 = await prisma.facility.upsert({
-      where: { name: 'Entertainment Systems' },
+      where: { id: 5, name: 'Entertainment Systems' },
       update: { name: 'Entertainment Systems' },
       create: { name: 'Entertainment Systems' },
     });
     const facility3 = await prisma.facility.upsert({
-      where: { name: 'Catering' },
+      where: { id: 6, name: 'Catering' },
       update: { name: 'Catering' },
       create: { name: 'Catering' },
     });
     const facility4 = await prisma.facility.upsert({
-      where: { name: 'Conference Facilties' },
+      where: { id: 7, name: 'Conference Facilties' },
       update: { name: 'Conference Facilties' },
       create: { name: 'Conference Facilties' },
     });
     const facility5 = await prisma.facility.upsert({
-      where: { name: 'Pet Friendly' },
+      where: { id: 8, name: 'Pet Friendly' },
       update: { name: 'Pet Friendly' },
       create: { name: 'Pet Friendly' },
     });
@@ -51,17 +51,17 @@ async function main() {
     console.log({ facility1, facility2, facility3, facility4, facility5 });
     // Capacity seed Data
     const capacity1 = await prisma.capacity.upsert({
-      where: { name: 'Small jet (Up to 6 passengers)' },
+      where: { id: 1, name: 'Small jet (Up to 6 passengers)' },
       update: { name: 'Small jet (Up to 6 passengers)' },
       create: { name: 'Small jet (Up to 6 passengers)' },
     });
     const capacity2 = await prisma.capacity.upsert({
-      where: { name: 'Midsize jet (7-10 passengers)' },
+      where: { id: 2, name: 'Midsize jet (7-10 passengers)' },
       update: { name: 'Midsize jet (7-10 passengers)' },
       create: { name: 'Midsize jet (7-10 passengers)' },
     });
     const capacity3 = await prisma.capacity.upsert({
-      where: { name: 'Large Jet (11-19 passengers)' },
+      where: { id: 3, name: 'Large Jet (11-19 passengers)' },
       update: { name: 'Large Jet (11-19 passengers)' },
       create: { name: 'Large Jet (11-19 passengers)' },
     });
